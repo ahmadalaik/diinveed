fd03d20a4b15a14aeb2a04993b9c9feb0aed6648
ahmadalaik <ahmadalaik.mu@gmail.com>
2026-05-15 10:47:30 +0700

    chore: add instance prisma client and baseline database

---
+++ lib/prisma.ts
@@ -0,0 +1,26 @@
+import "dotenv/config";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+import { PrismaClient } from "../generated/prisma/client";
+
+const adapter = new PrismaMariaDb({
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT ? Number(process.env.DATABASE_PORT) : undefined,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  connectionLimit: 5,
+});
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
+
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
