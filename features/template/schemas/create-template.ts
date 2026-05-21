afd8d089ebc4af6a0d8c2d909c6ca8ac215717ad
ahmadalaik <ahmadalaik.mu@gmail.com>
2026-05-21 12:29:12 +0700

    refactor!: separate create template form schema and action schema

--- features/template/schemas/create-template.ts
+++ features/template/schemas/create-template.ts
@@ -1,11 +1,38 @@
 import z from "zod";
 
-export const createTemplateSchema = z.object({
+const MAX_FILE_SIZE = 5 * 1024 * 1024;
+const ACCEPTED_IMAGE_TYPES = [
+  "image/jpeg",
+  "image/jpg",
+  "image/png",
+  "image/webp",
+];
+
+const baseTemplateSchema = z.object({
   name: z.string().min(1, "Nama wajib diisi"),
-  category: z.string().min(1, "Kategory wajib diisi"),
+  category: z.string().min(1, "Kategori wajib diisi"),
   description: z.string().optional(),
-  thumbnailUrl: z.url("Thumbnail wajib diisi."),
   status: z.enum(["active", "draft", "archived"]),
 });
 
-export type CreateTemplateType = z.infer<typeof createTemplateSchema>;
+export const createTemplateFormSchema = baseTemplateSchema.extend({
+  thumbnail: z
+    .instanceof(File, { message: "Silahkan upload thumbnail" })
+    .refine((file) => file.size <= MAX_FILE_SIZE, {
+      message: "Ukuran thumbnail maksimal 5MB",
+    })
+    .refine((file) => ACCEPTED_IMAGE_TYPES.includes(file.type), {
+      message:
+        "Format file tidak didukung (Hanya diizinkan: .jpg, .jpeg, .png, .webp)",
+    }),
+});
+
+export const createTemplateActionSchema = baseTemplateSchema.extend({
+  thumbnailUrl: z.url("Thumbnail wajib diisi."),
+});
+
+export type CreateTemplateFormType = z.infer<typeof createTemplateFormSchema>;
+
+export type CreateTemplateActionType = z.infer<
+  typeof createTemplateActionSchema
+>;
