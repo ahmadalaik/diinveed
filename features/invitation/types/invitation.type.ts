5b20b1fbc00cdcf943b603d6f600f505863fa2b9
ahmadalaik <ahmadalaik.mu@gmail.com>
2026-05-25 19:40:17 +0700

    feat(invitation): add invitation types

---
+++ features/invitation/types/invitation.type.ts
@@ -0,0 +1,61 @@
+export type RsvpOptions = {
+  accept: boolean;
+  decline: boolean;
+  maybe: boolean;
+  plusOne: boolean;
+};
+
+export type EventItem = {
+  time: string;
+  title: string;
+  description: string;
+};
+
+export type StoryItem = {
+  year: string;
+  title: string;
+  body: string;
+};
+
+export type GiftItem = {
+  name: string;
+  description: string;
+};
+
+export type InvitationState = {
+  id: string;
+  userId: string;
+  token: string;
+  title: string;
+  subtitle: string;
+  date: string;
+  time: string;
+  hosts: string;
+  message: string;
+  venueName: string;
+  venueAddress: string;
+  coverImage: string | null;
+  templateId: string;
+  paletteIdx: number | null;
+  fontId: string;
+  backgroundType: string;
+  dressCode: string;
+  rsvpDeadline: string;
+  rsvpOptions: RsvpOptions;
+  events: EventItem[];
+  stories: StoryItem[];
+  gallery: string[];
+  stickers: string[];
+  gifts: GiftItem[];
+  isPublished: boolean;
+};
+
+export type GuestRsvpRow = {
+  id: string;
+  invitationId: string;
+  name: string;
+  email: string | null;
+  response: "ACCEPT" | "DECLINE" | "MAYBE";
+  plusOne: boolean;
+  submittedAt: Date;
+};
