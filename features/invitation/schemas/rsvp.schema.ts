0db1be97f5ec9e8d7241189630137e740a99b5e6
ahmadalaik <ahmadalaik.mu@gmail.com>
2026-05-25 21:20:02 +0700

    refactor(invitation): update key submitRsvpSchema from responses to response

--- features/invitation/schemas/rsvp.schema.ts
+++ features/invitation/schemas/rsvp.schema.ts
@@ -3,7 +3,7 @@ import z from "zod";
 export const submitRsvpSchema = z.object({
   name: z.string().min(1, "Nama wajib diisi"),
   email: z.string().optional(),
-  responses: z.enum(["ACCEPT", "DECLINE", "MAYBE"]),
+  response: z.enum(["ACCEPT", "DECLINE", "MAYBE"]),
   plusOne: z.boolean().default(false),
 });
 
