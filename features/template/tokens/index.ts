ec5506b6362441982e3f332d819476da6afb054f
ahmadalaik <ahmadalaik.mu@gmail.com>
2026-05-26 20:52:27 +0700

    feat(tokens): add tokens barrel export

---
+++ features/template/tokens/index.ts
@@ -0,0 +1,8 @@
+export type { InvitationToken, BorderRadius, TokenOverrides } from "./types";
+export {
+  TOKENS,
+  DEFAULT_TOKEN_ID,
+  getToken,
+  mergeTokenOverrides,
+} from "./registry";
+export { applyTokens, BORDER_RADIUS_MAP } from "./apply";
