87be3bc6c830e2d4435ebca48aa504e4893c09c0
ahmadalaik <ahmadalaik.mu@gmail.com>
2026-06-12 23:20:13 +0700

    refactor(invitation): drop legacy title/date/host/dress columns

---
+++ prisma/migrations/20260609130000_delete_invitation_dress_code_column/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `dress_code` on the `invitations` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `invitations` DROP COLUMN `dress_code`;
