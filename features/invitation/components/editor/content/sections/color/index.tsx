a0b2a9ab380cabb40f4ac3caad332fd60b6b8475
ahmadalaik <ahmadalaik.mu@gmail.com>
2026-07-20 02:35:27 +0700

    feat(invitation-editor): update color section

--- features/invitation/components/editor/content/sections/color/index.tsx
+++ features/invitation/components/editor/content/sections/color/index.tsx
@@ -1,8 +1,4 @@
-"use client";
-
 import { Accordion } from "@/components/ui/accordion";
-import { useInvitationStore } from "@/features/invitation/store/invitation-store";
-import { TamanNusantaraPresets } from "../design/taman-nusantara-presets";
 import {
   ColorBackgroundField,
   ColorButtonField,
@@ -10,12 +6,6 @@ import {
 } from "./color-field";
 
 export function ColorSection() {
-  const templateSlug = useInvitationStore((state) => state.templateSlug);
-
-  if (templateSlug === "taman-nusantara") {
-    return <TamanNusantaraPresets kind="palette" />;
-  }
-
   return (
     <Accordion
       type="multiple"
