fc76de2c8eb43505ae14bec48ae7f7e73bb5a88d
ahmadalaik <ahmadalaik.mu@gmail.com>
2026-07-20 02:04:14 +0700

    feat(template): update template token

--- features/template/tokens/index.ts
+++ features/template/tokens/index.ts
@@ -9,28 +9,9 @@ export type {
 export { templateCssVars } from "./css-vars";
 export { kalandraTokens } from "./kalandra";
 export { dikaraTokens } from "./dikara";
-export { agnimayaTokens } from "./agnimaya";
-export { terracottaTokens } from "./adhikari";
 export { pradiptaTokens } from "./pradipta";
-export {
-  pavanaDefaultOverlayVariant,
-  pavanaOverlayVariants,
-  pavanaTokens,
-} from "./pavana";
 export {
   TEMPLATE_TOKENS,
   getTemplateTokens,
   mergeTemplateTokenOverrides,
 } from "./template-tokens";
-export { sakuraReverieTokens } from "./sakura-reverie";
-export { sakuraReverieCodexTokens } from "./sakura-reverie-codex";
-export { tamanNusantaraTokens } from "./taman-nusantara";
-export { larasatiTokens } from "./larasati";
-export { javaneseEngravingTokens } from "./javanese-engraving";
-export { arunikaTokens } from "./arunika";
-export {
-  tamanPalettePresets,
-  tamanTypographyPresets,
-  type TamanPalettePreset,
-  type TamanTypographyPreset,
-} from "./taman-nusantara-presets";
